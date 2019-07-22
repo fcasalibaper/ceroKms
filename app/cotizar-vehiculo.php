ef22226b9e77b990356dbbffd6e73458ae798930
fcasalibaper <fernando.casaliba@fravega.com.ar>
2019-07-22 18:14:55 -0300

    selector de financiacion

--- app/cotizar-vehiculo.php
+++ app/cotizar-vehiculo.php
@@ -4,6 +4,7 @@
   $porcentaje = 40;
 ?>
 <!DOCTYPE html>
+
 <head>
   <?php include("includes/head.inc.html"); ?>
 </head>
@@ -15,23 +16,84 @@
   <section class="cotizador">
     <div class="container containerBig">
       <div class="row">
-        
-      <?php include("includes/menuCotizador.inc.php"); ?>
 
-      <?php include("includes/titleSections.inc.php"); ?>
+        <?php include("includes/menuCotizador.inc.php"); ?>
 
-      <aside class="timeline">
-        <div class="progress">
+        <?php include("includes/titleSections.inc.php"); ?>
 
-          <div class="progress-points">
-            <div class="point point-1 <?php if ($porcentaje > 0) echo 'active'; ?>"></div>
-            <div class="point point-2 <?php if ($porcentaje >= 50) echo 'active'; ?>"></div>
-            <div class="point point-3 <?php if ($porcentaje >= 100) echo 'active'; ?>"></div>
+        <!-- timeline -->
+        <aside class="timeline">
+          <div class="progress">
+
+            <div class="progress-points">
+              <div class="point point-1 <?php if ($porcentaje > 0) echo 'active'; ?>"></div>
+              <div class="point point-2 <?php if ($porcentaje >= 50) echo 'active'; ?>"></div>
+              <div class="point point-3 <?php if ($porcentaje >= 100) echo 'active'; ?>"></div>
+            </div>
+
+            <div class="progress-bar" role="progressbar" style="width: <?php echo $porcentaje;?>%"
+              aria-valuenow="<?php echo $porcentaje;?>" aria-valuemin="0" aria-valuemax="100">
+              <span><?php echo $porcentaje;?></span></div>
           </div>
+        </aside>
 
-          <div class="progress-bar" role="progressbar" style="width: <?php echo $porcentaje;?>%" aria-valuenow="<?php echo $porcentaje;?>" aria-valuemin="0" aria-valuemax="100"><span><?php echo $porcentaje;?></span></div>
-        </div>
-      </aside>
+        <!-- finanSelected -->
+        <section class="finanSelected">
+          <h4>Financiación seleccionada</h4>
+          <div class="finanSelected__item">
+            <span class="finanSelected__item__text">
+              Crédito normal
+            </span>
+            <span class="finanSelected__item__text">
+              Monto: $1.000.000
+            </span>
+            <span class="finanSelected__item__text">
+              12 cuotas de $2.799.807, 49
+            </span>
+            <a href="#" class="btn">
+              Cambiar
+            </a>
+          </div>
+        </section>
+
+        <section class="selectorFina">
+          <h4 class="title">Seleccione la financiación del monto</h4>
+
+          <span class="selectorFina__detail">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum consequuntur, distinctio sapiente, maxime explicabo beatae voluptatibus error corporis odit quam nesciunt deserunt, incidunt doloremque atque ea ducimus neque maiores eius.
+            <br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At nisi consequatur reiciendis modi debitis aperiam itaque magnam tempora?
+          </span>
+
+          <div class="selectorFina__items">
+            <div class="selectorFina__items__boxes">
+              <span class="text">Su Plan</span>
+              <div class="box">
+                <span class="active">12 cuotas de $2.799.807,49</span>
+              </div>
+            </div>
+            <div class="selectorFina__items__boxes">
+              <span class="text">Crédito Normal</span>
+              <div class="box">
+                <span>12 cuotas de $2.799.807,49</span>
+                <span>12 cuotas de $2.799.807,49</span>
+                <span>12 cuotas de $2.799.807,49</span>
+                <span>12 cuotas de $2.799.807,49</span>
+              </div>
+            </div>
+            <div class="selectorFina__items__boxes">
+              <span class="text">Crédito Inteligente</span>
+              <div class="box active">
+                <span>
+                24/36 Cuotas
+                <a href="#">Consultar ></a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <button class="btn btn btn--gradient btn--alignRight">Siguiente</button>
 
       </div>
     </div>
